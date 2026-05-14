57a159dbcebf14d005e9c38e189289798278c2ce
ReddyBhanuTeja <rteja@taatvam.com>
2026-05-14 16:17:46 +0530

    Bhanuteja Work2

--- src/component/Home/home.jsx
+++ src/component/Home/home.jsx
@@ -398,7 +398,7 @@ export default function Home() {
           </p>
         </footer>
 
-      </main>
+      </main> 
 
       {dialog.open && (
         <div className="dlg-overlay" onClick={() => setDialog({ open: false, name: '' })}>
